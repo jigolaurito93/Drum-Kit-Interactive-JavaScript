7e2a409283e24d83eb821cc9d6e1c9710b90604f
Jose Laurito <jigolaurito93@yahoo.com>
2023-08-10 01:17:00 -0500

    used switch statements to play audio

--- index.js
+++ index.js
@@ -1,6 +1,68 @@
-// Target the first button and added an event listener which is the click event and invoke the handleClick function
-document.querySelector('button').addEventListener('click',handleClick)
+// Get the length of the buttons
+var numberOfDrumButtons = document.querySelectorAll('.drum').length
 
-function handleClick() {
-    alert('I got clicked!');
+
+
+// For loop to loop through the buttons and use addEventListener of click event 
+for (var i = 0; i < numberOfDrumButtons; i++) {
+    document.querySelectorAll('.drum')[i].addEventListener('click',function(){
+
+        var buttonInnerHTML = this.innerHTML;
+
+        switch (buttonInnerHTML) {
+            case 'w':
+                var crash = new Audio('/sounds/crash.mp3');
+                crash.play();
+            break;
+
+            case 'a':
+                var kickBass = new Audio('/sounds/kick-bass.mp3');
+                kickBass.play();
+            break;
+
+            case 's':
+                var snare = new Audio('/sounds/snare.mp3');
+                snare.play();
+            break;
+
+            case 'd':
+                var tom1 = new Audio('/sounds/tom-1.mp3');
+                tom1.play();
+            break;
+
+            case 'j':
+                var tom2 = new Audio('/sounds/tom-2.mp3');
+                tom2.play();
+            break;
+
+            case 'k':
+                var tom3 = new Audio('/sounds/tom-3.mp3');
+                tom3.play();
+            break;
+
+            case 'l':
+                var tom4 = new Audio('/sounds/tom-4.mp3');
+                tom4.play();
+            break;
+        
+            default:
+                break;
+        }
+    })
 }
+
+// var audio = new Audio('/sounds/tom-1.mp3');
+// audio.play();
+
+
+
+
+// function HouseKeeper (yearsOfExperience, name, cleaningRepertoire){
+//     this.yearsOfExperience = yearsOfExperience;
+//     this.name = name;
+//     this.cleaningRepertoire = cleaningRepertoire;
+// }
+
+// var HouseKeeper1 = new HouseKeeper(12, 'Jane', ['bathroom', 'lobby', 'bedroom'])
+
+// console.log(HouseKeeper1.cleaningRepertoire[1]); 
